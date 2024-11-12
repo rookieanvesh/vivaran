70406045a3ddef210a74518e1c2fec0eb29f270f
rookieanvesh <srivastavaanvesh13@gmail.com>
2024-11-12 23:16:46 +0530

    init commit

---
+++ src/main/java/com/secure/vivaran/VivaranApplication.java
@@ -0,0 +1,13 @@
+package com.secure.vivaran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivaranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VivaranApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secure/vivaran/VivaranApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secure.vivaran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VivaranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
