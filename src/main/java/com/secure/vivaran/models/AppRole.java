02a262c00a206998d83d6031f5e719b0914c96c4
rookieanvesh <srivastavaanvesh13@gmail.com>
2024-11-14 22:30:10 +0530

    userdetails and userdetailsservice

---
+++ src/main/java/com/secure/vivaran/models/AppRole.java
@@ -0,0 +1,6 @@
+package com.secure.vivaran.models;
+
+public enum AppRole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/secure/vivaran/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.secure.vivaran.security.services;
+
+import com.secure.vivaran.models.User;
+import com.secure.vivaran.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+
+
+
+
+}
