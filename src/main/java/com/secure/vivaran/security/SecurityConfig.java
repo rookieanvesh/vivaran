a951425bb496adfbd34a8e1e85ec48514788a09e
rookieanvesh <srivastavaanvesh13@gmail.com>
2024-11-14 23:54:41 +0530

    removing custom users

--- src/main/java/com/secure/vivaran/security/SecurityConfig.java
+++ src/main/java/com/secure/vivaran/security/SecurityConfig.java
@@ -5,12 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -25,28 +20,6 @@ public class SecurityConfig {
         http.httpBasic(withDefaults());
         //returning the object
         return http.build();
-    }
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        JdbcUserDetailsManager manager =
-                new JdbcUserDetailsManager(dataSource);
-        if (!manager.userExists("user1")) {
-            manager.createUser(
-                    User.withUsername("user1")
-                            .password("{noop}password1")
-                            .roles("USER")
-                            .build()
-            );
-        }
-        if (!manager.userExists("admin")) {
-            manager.createUser(
-                    User.withUsername("admin")
-                            .password("{noop}adminPass")
-                            .roles("ADMIN")
-                            .build()
-            );
-        }
-        return manager;
     }
 
 }
