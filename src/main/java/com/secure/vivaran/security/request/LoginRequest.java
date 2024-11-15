64836d15aa17f85621d39abde6a93b5077ffdc76
rookieanvesh <srivastavaanvesh13@gmail.com>
2024-11-16 03:29:10 +0530

    request and response

---
+++ src/main/java/com/secure/vivaran/security/request/LoginRequest.java
@@ -0,0 +1,12 @@
+package com.secure.vivaran.security.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LoginRequest {
+    private String username;
+    private String password;
+
+}
