203f0c6d4f8ba3ea7ff57b63dfbbbf651ff5dcbf
rookieanvesh <srivastavaanvesh13@gmail.com>
2024-11-17 23:29:17 +0530

    changed a few comments

--- src/main/java/com/secure/vivaran/security/services/UserDetailsImpl.java
+++ src/main/java/com/secure/vivaran/security/services/UserDetailsImpl.java
@@ -16,13 +16,12 @@ import java.util.Objects;
 @Data
 public class UserDetailsImpl implements UserDetails {
     private static final long serialVersionUID = 1L;
-//for the current user, this class helps spring security to understand and register our custom user entity
+    //for the current user, this class helps spring security to understand and register our custom user entity
     private Long id;
     private String username;
     private String email;
 
-    @JsonIgnore
-    //added this to ignore password field when interacting with APIs
+    @JsonIgnore//added this to ignore password field when interacting with APIs
     private String password;
 
     private boolean is2faEnabled;
