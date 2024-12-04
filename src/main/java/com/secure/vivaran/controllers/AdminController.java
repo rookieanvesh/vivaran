0529fc05ae75a6e2436a6181b346b3aef628715a
Anvesh Srivastava <132641928+rookieanvesh@users.noreply.github.com>
2024-12-04 23:30:27 +0530

    Update AdminController.java

--- src/main/java/com/secure/vivaran/controllers/AdminController.java
+++ src/main/java/com/secure/vivaran/controllers/AdminController.java
@@ -10,7 +10,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 @RestController
 @RequestMapping("/api/admin")
 @PreAuthorize("hasRole('ROLE_ADMIN')")
