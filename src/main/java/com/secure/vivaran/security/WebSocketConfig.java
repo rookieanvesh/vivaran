277d51f4412e34b25ca87bd1646d77de0b41a134
Anvesh Srivastava <srivastavaanvesh13@gmail.com>
2025-01-23 23:33:49 +0530

    Update WebSocketConfig.java

--- src/main/java/com/secure/vivaran/security/WebSocketConfig.java
+++ src/main/java/com/secure/vivaran/security/WebSocketConfig.java
@@ -10,17 +10,5 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic", "/user");
-        config.setApplicationDestinationPrefixes("/app");
-        config.setUserDestinationPrefix("/user");
-    }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000")
-                .withSockJS();
-    }
+    
 }
